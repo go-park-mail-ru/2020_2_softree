ccfd1bafc7c04ed794dcdf3fd9daedbe68d90d5c
thePsina <mishele20@mail.ru>
2020-10-05 01:47:27 +0300

    cookie reworked

--- infrastructure/security/cookie.go
+++ infrastructure/security/cookie.go
@@ -8,9 +8,12 @@ import (
 func MakeCookie(valueForCookie string, domain string) http.Cookie {
 	expiration := time.Now().Add(10 * 24 * time.Hour)
 	return http.Cookie{
-		Name:    "session_id",
-		Value:   MakeShieldedHash(valueForCookie),
-		Expires: expiration,
-		Path:    "/",
+		Name:     "session_id",
+		Value:    MakeShieldedHash(valueForCookie),
+		Expires:  expiration,
+		SameSite: http.SameSiteNoneMode,
+		Secure:   true,
+		HttpOnly: true,
+		Path:     "/",
 	}
 }
