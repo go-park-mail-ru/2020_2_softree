a798c496dac999b1c5000bdcf7dbfbd2b37b721d
thePsina <mishele20@mail.ru>
2020-10-05 01:16:38 +0300

    add utils

--- handlers/authorization/logout.go
+++ handlers/authorization/logout.go
@@ -2,18 +2,19 @@ package authorization
 
 import (
 	"net/http"
+	"server/handlers/authorization/utils"
 	"time"
 )
 
 func Logout(w http.ResponseWriter, r *http.Request) {
 	session, err := r.Cookie("session_id")
 	if err == http.ErrNoCookie {
-		http.Redirect(w, r, RootPage, http.StatusFound)
+		http.Redirect(w, r, utils.RootPage, http.StatusFound)
 		return
 	}
 
 	session.Expires = time.Now().AddDate(0, 0, -1)
 	http.SetCookie(w, session)
 
-	http.Redirect(w, r, RootPage, http.StatusFound)
+	http.Redirect(w, r, utils.RootPage, http.StatusFound)
 }
