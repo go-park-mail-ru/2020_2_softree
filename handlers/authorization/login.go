42b6a0702313a2d33d2cde82e4811c13c4223c07
thePsina <mishele20@mail.ru>
2020-10-04 18:09:54 +0300

    resolved

--- handlers/authorization/login.go
+++ handlers/authorization/login.go
@@ -25,28 +25,29 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if UsersServerSession[loginJSON.Email] != security.MakeShieldedHash(loginJSON.Password) {
-	if _, exist := UsersServerSession[loginJSON.Email]; !exist {
-		var errorJSON entity.ErrorJSON
+		if _, exist := UsersServerSession[loginJSON.Email]; !exist {
+			var errorJSON entity.ErrorJSON
 
-		errorJSON.Email = append(errorJSON.Email, "user does not exist")
-		result, err := json.Marshal(errorJSON)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			errorJSON.Email = append(errorJSON.Email, "user does not exist")
+			result, err := json.Marshal(errorJSON)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Write(result)
+			http.Redirect(w, r, SignupPage, http.StatusBadRequest)
+
 			return
 		}
-		w.Write(result)
-		http.Redirect(w, r, SignupPage, http.StatusBadRequest)
 
-		return
+		if UsersServerSession[loginJSON.Email] != security.MakeShieldedHash(loginJSON.Password) {
+			http.Redirect(w, r, LoginPage, http.StatusBadRequest)
+
+			return
+		}
+
+		cookie := security.MakeCookie(loginJSON.Email, r.Header.Get("Origin"))
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, RootPage, http.StatusFound)
 	}
-
-	if UsersServerSession[loginJSON.Email] != security.MakeShieldedHash(loginJSON.Password) {
-		http.Redirect(w, r, LoginPage, http.StatusBadRequest)
-
-		return
-	}
-
-	cookie := security.MakeCookie(loginJSON.Email, r.Header.Get("Origin"))
-	http.SetCookie(w, &cookie)
-	http.Redirect(w, r, RootPage, http.StatusFound)
 }
