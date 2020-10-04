1f11e56306c9e8a14aa8d33a84cb2c883fbc5c9e
thePsina <mishele20@mail.ru>
2020-10-04 15:21:48 +0300

    rework

--- Handlers/RatesInteraction/Rates.go
+++ Handlers/RatesInteraction/Rates.go
@@ -1,42 +1,11 @@
-package UserInteraction
+package RatesInteraction
 
 import (
 	"encoding/json"
 	"net/http"
 	"server/Domain/Entity"
-	"server/Handlers/Authorization"
 	"strconv"
 )
-
-const (
-	testID    = 123
-	testEmail = "hound@psina.ru"
-)
-
-func UserData(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	_, err := r.Cookie("session_id")
-	logged := err != http.ErrNoCookie
-
-	var u Entity.User
-	u.ID = testID
-	u.Email = testEmail
-	result, e := json.Marshal(u)
-	if e != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if logged {
-		w.Write(result)
-		w.WriteHeader(http.StatusOK)
-	} else {
-		http.Redirect(w, r, Authorization.SignupPage, http.StatusUnauthorized)
-	}
-}
 
 func Rates(w http.ResponseWriter, r *http.Request) {
 	result := make([]byte, 0)
