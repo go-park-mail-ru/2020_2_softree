67f9347a0022774cf70e70655f727f539c95f42b
Mark Bykhovets <mb@hrsmnt.ru>
2020-10-06 00:22:15 +0300

    add options to allowed header

--- main.go
+++ main.go
@@ -23,14 +23,14 @@ func main() {
 	router := mux.NewRouter()
 	r := router.PathPrefix("").Subrouter()
 
-	r.HandleFunc("/signin", login.Login).Methods("POST")
-	r.HandleFunc("/signup", signup.Signup).Methods("POST")
-	r.HandleFunc("/auth", auth.Authentication).Methods("GET")
+	r.HandleFunc("/signin", login.Login).Methods("POST", "OPTIONS")
+	r.HandleFunc("/signup", signup.Signup).Methods("POST", "OPTIONS")
+	r.HandleFunc("/auth", auth.Authentication).Methods("GET", "OPTIONS")
 	r.HandleFunc("/logout", logout.Logout)
-	r.HandleFunc("/user-data", userInteraction.UserData).Methods("POST")
-	r.HandleFunc("/rates", ratesInteraction.Rates).Methods("GET")
+	r.HandleFunc("/user-data", userInteraction.UserData).Methods("POST", "OPTIONS")
+	r.HandleFunc("/rates", ratesInteraction.Rates).Methods("GET", "OPTIONS")
 	// r.HandleFunc("/rates/{id:([1-9]0?)+}", ratesInteraction.Rates).Methods("GET")
-	r.HandleFunc("/user", userInteraction.UpdateUser).Methods("PUT", "PATCH")
+	r.HandleFunc("/user", userInteraction.UpdateUser).Methods("PUT", "PATCH", "OPTIONS")
 	r.Use(corsInteraction.CORSMiddleware())
 
 	server := &http.Server{
