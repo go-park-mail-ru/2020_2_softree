2d9cc87592cab625ab52288d745487e4b37d3d7c
thePsina <mishele20@mail.ru>
2020-10-06 01:25:06 +0300

    resolved

--- handlers/authorization/logout/logout.go
+++ handlers/authorization/logout/logout.go
@@ -8,22 +8,11 @@ import (
 func Logout(w http.ResponseWriter, r *http.Request) {
 	session, err := r.Cookie("session_id")
 	if err == http.ErrNoCookie {
-<<<<<<< HEAD
-		w.Header().Set("Location", utils.RootPage)
-		w.WriteHeader(http.StatusOK)
-=======
 		w.WriteHeader(http.StatusFound)
->>>>>>> origin/michael
 		return
 	}
 
 	session.Expires = time.Now().AddDate(0, 0, -1)
 	http.SetCookie(w, session)
-<<<<<<< HEAD
-
-	w.Header().Set("Location", utils.RootPage)
-	w.WriteHeader(http.StatusOK)
-=======
 	w.WriteHeader(http.StatusFound)
->>>>>>> origin/michael
 }
