a7dcb3fa3326eb0fc0a888a6a09fdf95751e7bbe
thePsina <mishele20@mail.ru>
2020-10-21 14:25:05 +0300

    cookie interface

--- src/infrastructure/auth/auth.go
+++ src/infrastructure/auth/auth.go
@@ -1,5 +1,10 @@
 package auth
 
+import (
+	"net/http"
+	"time"
+)
+
 type AuthInterface interface {
 	CreateAuth(uint64, *CookieDetails) error
 	CheckAuth(string) (uint64, error)
@@ -7,9 +12,27 @@ type AuthInterface interface {
 }
 
 type CookieDetails struct {
+	Name     string
+	Value    string
+	Expires  time.Time
+	Domain   string
+	Secure   bool
+	HttpOnly bool
+	Path     string
 }
 
 type AccessDetails struct {
-	SessionId string
-	UserId    uint64
+	SessionId string `json:"session_id"`
+}
+
+func MakeCookieDetailsFromCookie(cookie http.Cookie) CookieDetails {
+	return CookieDetails{
+		Name:     cookie.Name,
+		Value:    cookie.Value,
+		Expires:  cookie.Expires,
+		Domain:   cookie.Domain,
+		Secure:   cookie.Secure,
+		HttpOnly: cookie.HttpOnly,
+		Path:     cookie.Path,
+	}
 }
--- src/infrastructure/auth/cookie.go
+++ src/infrastructure/auth/cookie.go
@@ -1,6 +1,15 @@
 package auth
 
-import "net/http"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"server/src/infrastructure/config"
+	"strconv"
+	"time"
+)
 
 type CookieInterface interface {
 	CreateCookie(uint64) (*CookieDetails, error)
@@ -9,10 +18,37 @@ type CookieInterface interface {
 
 type Cookie struct{}
 
-func (c *Cookie) CreateCookie(userId uint64) (*CookieDetails, error) {
-	return &CookieDetails{}, nil
+func (c *Cookie) CreateCookie() (*CookieDetails, error) {
+	hash, err := makeCookieHash()
+	if err != nil {
+		return &CookieDetails{}, err
+	}
+
+	cd := MakeCookieDetailsFromCookie(http.Cookie{
+		Name:     "session_id",
+		Value:    hash,
+		Expires:  time.Now().Add(24 * time.Hour),
+		Domain:   config.GlobalServerConfig.Domain,
+		Secure:   config.GlobalServerConfig.Secure,
+		HttpOnly: true,
+		Path:     "/",
+	})
+
+	return &cd, nil
 }
 
-func (c *Cookie) ExtractData(r *http.Request) (*AccessDetails, error) {
-	return &AccessDetails{}, nil
+func (c *Cookie) ExtractData(r *http.Request) (ad *AccessDetails, err error) {
+	if err := json.NewDecoder(r.Body).Decode(&ad); err != nil {
+		return &AccessDetails{}, err
+	}
+	return ad, nil
+}
+
+func makeCookieHash() (string, error) {
+	hash := sha256.New()
+
+	if _, err := hash.Write([]byte(strconv.Itoa(rand.Int()))); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(hash.Sum(nil)), nil
 }
