d54a20a7621deded8843b6b851939e5ec66b5c07
thePsina <mishele20@mail.ru>
2020-10-29 16:47:33 +0300

    mock tests

--- src/infrastructure/persistence/userMemoryRepo_test.go
+++ src/infrastructure/persistence/userMemoryRepo_test.go
@@ -28,33 +28,8 @@ func TestUserMemoryRepo_SaveUserSuccess(t *testing.T) {
 	mock := userMock.NewUserRepositoryForMock(ctrl)
 	mock.EXPECT().SaveUser(userToSave).Times(1).Return(expectedUser, nil)
 
-	handler := application.UserApp{ur: mock}
-	receivedUser, err := handler.ur.SaveUser(userToSave)
-
-	require.NoError(t, err)
-	require.Equal(t, expectedUser, receivedUser)
-}
-
-func TestUserMemoryRepo_UpdateUserSuccess(t *testing.T) {
-	userToSave := entity.User{
-		Email: "yandex@mail.ru",
-		Password: "password",
-	}
-	password, _ := security.MakeShieldedHash(userToSave.Password)
-	expectedUser := entity.User{
-		ID: 1,
-		Email: "yandex@mail.ru",
-		Password: password,
-	}
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mock := userMock.NewUserRepositoryForMock(ctrl)
-	mock.EXPECT().SaveUser(userToSave).Times(1).Return(expectedUser, nil)
-
-	handler := application.UserApp{ur: mock}
-	receivedUser, err := handler.ur.SaveUser(userToSave)
+	handler := application.NewUserApp(mock)
+	receivedUser, err := handler.SaveUser(userToSave)
 
 	require.NoError(t, err)
 	require.Equal(t, expectedUser, receivedUser)
