9b08dbc0694b916ef1819cc2b514b17865f6eafa
thePsina <mishele20@mail.ru>
2020-11-13 22:11:28 +0300

    fix

--- src/interfaces/profile/profile.go
+++ src/interfaces/profile/profile.go
@@ -2,17 +2,17 @@ package profile
 
 import (
 	"github.com/microcosm-cc/bluemonday"
-	"github.com/sirupsen/logrus"
 	"server/src/application"
+	"server/src/infrastructure/log"
 )
 
 type Profile struct {
 	userApp   application.UserApp
 	auth      application.UserAuth
-	log       logrus.Logger
+	log       log.LogHandler
 	sanitizer bluemonday.Policy
 }
 
-func NewProfile(uApp application.UserApp, auth application.UserAuth, log *logrus.Logger) *Profile {
-	return &Profile{userApp: uApp, auth: auth, log: *log, sanitizer: *bluemonday.UGCPolicy()}
+func NewProfile(uApp application.UserApp, auth application.UserAuth, log log.LogHandler) *Profile {
+	return &Profile{userApp: uApp, auth: auth, log: log, sanitizer: *bluemonday.UGCPolicy()}
 }
