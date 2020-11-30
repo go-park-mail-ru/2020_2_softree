77b63ab74047ede5e47bcc1e4e959748ab5054da
thePsina <mishele20@mail.ru>
2020-11-30 17:03:25 +0300

    session fix

--- profile/pkg/infrastructure/persistence/income.go
+++ profile/pkg/infrastructure/persistence/income.go
@@ -1,1 +1,10 @@
 package persistence
+
+import (
+	"context"
+	profile "server/profile/pkg/profile/gen"
+)
+
+func (managerDB *UserDBManager) GetIncome(c context.Context, in *profile.IncomeParameters) (*profile.Income, error) {
+	return &profile.Income{}, nil
+}
